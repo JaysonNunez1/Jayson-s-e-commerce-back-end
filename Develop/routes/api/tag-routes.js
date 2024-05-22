8b5d28f3c448bb638560771a2eba685e071be282
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-05-22 08:33:39 -0400

    updated tag name by its id and delete on tag by its id value

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -70,11 +70,48 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
+  tag.update(
+    {
+      tag_name: req.body.tag_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+    .then(Data =>{
+      if (!Data) {
+        res.status(404).json({ message: 'No tag found with this id!' });
+        return;
+      }
+
+      res.json(Data);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+   });
+   
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+      }
+      })
+      .then(Data => {
+        if (!Data) {
+          res.status(404).json({ message: 'No tag found with this id!' });
+          return;
+          }
+          res.json(Data);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+    });
 });
 
 module.exports = router;
